0ae8f90bb55a2d2ecc06b88c6c35749c9ce4c523
hiroya12221023 <a1211065@kuins.ac.jp>
2023-10-26 05:14:15 +0000

    y

--- routes/web.php
+++ routes/web.php
@@ -37,3 +37,4 @@ Route::get('/write', function () {
 
 Route::post('/generate', [ArticleGeneratorController::class, 'index']);
 Route::post('/show-answer', [QuizController::class, 'showanswer']);
+Route::post('/show-answeraaaaaaaa', [QuizController::class, 'showanswer']);
